44893a764971e060bda2c9bab55eb187b0b7e8ab
William Villeneuve <william@checkout51.com>
2017-03-05 15:41:28 -0500

    Added ssh and server-data configs

---
+++ frontend/config.sh
@@ -0,0 +1,6 @@
+
+__private_config()
+{
+	__priv_conf[ssh_configs]="$source_dir/$category/ssh"
+}
+__private_config
---
+++ server-data/config.sh
@@ -0,0 +1,12 @@
+
+__private_config()
+{
+	__priv_conf[samba_password]="PasswordHere1234"
+
+	__priv_conf[deluge_password]="PasswordHere1234"
+
+	__priv_conf[ddclient_cf_login]="login@example.com"
+	__priv_conf[ddclient_cf_password]="PasswordHere1234"
+	__priv_conf[ddclient_cf_domain]="domain.com"
+}
+__private_config
