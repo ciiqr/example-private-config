221c51a1af0c5837afc0b2c45d67e59295e02b5d
William Villeneuve <william@checkout51.com>
2017-03-08 22:18:31 -0500

    Added ddclient cf zone and git email

---
+++ development/config.sh
@@ -0,0 +1,6 @@
+
+__private_config()
+{
+	__priv_conf[git_email]="me@domain.com"
+}
+__private_config
--- server/config.sh
+++ server/config.sh
@@ -1,7 +1,7 @@
 
 __private_config()
 {
-	# TODO: use these...
+	__priv_conf[ddclient_cf_zone]="domain.com"
 	__priv_conf[ddclient_cf_login]="login@example.com"
 	__priv_conf[ddclient_cf_password]="PasswordHere1234"
 }
